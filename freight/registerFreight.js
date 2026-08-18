c5cbcfbab5251d6ee5e579090d4e4c610dbfaf8b
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 19:55:47 -0500

    Mount route calculation with Freight subsystem

--- freight/registerFreight.js
+++ freight/registerFreight.js
@@ -2,6 +2,7 @@
 
 const { createFreightRouter } = require("./freightRouter");
 const { createAssetMoveRouter, ensureAssetMoveSchema } = require("./assetMoveRouter");
+const { createFreightRouteRouter } = require("./routeRouter");
 const { ensureFreightSchema } = require("./freightRepository");
 const { createAdapters } = require("./integrationAdapters");
 
@@ -12,6 +13,7 @@ async function initializeFreightSubsystem(pool) {
 
 function registerFreightSubsystem(app, { pool, resolveActor = null, adapters = createAdapters() } = {}) {
   if (!app || !pool) throw new Error("registerFreightSubsystem requires app and pool.");
+  app.use("/freight/v1/routes", createFreightRouteRouter());
   app.use("/freight/v1", createFreightRouter({ pool, resolveActor, adapters }));
   app.use("/asset-moves/v1", createAssetMoveRouter({ pool, resolveActor, adapters }));
   return { initialize: () => initializeFreightSubsystem(pool) };
