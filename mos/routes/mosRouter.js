e0140ce8b7a182d8ffe12d53cdd553d3dc70567f
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-08 23:04:32 -0500

    Revert unverified concurrent batch admission

--- mos/routes/mosRouter.js
+++ mos/routes/mosRouter.js
@@ -1527,12 +1527,13 @@ router.post(
         );
       }
 
-      const admissions = await Promise.all(
-        requests.map(async request => ({
+      const admissions = [];
+      for (const request of requests) {
+        admissions.push({
           ok: true,
           ...(await admitAuthorizedCanonicalIdentity(req, request))
-        }))
-      );
+        });
+      }
 
       return res.json({
         ok: true,
